ef8ea76789aafdacff7d3a18d6ff6b8762cbd734
inealey <isaacnealey@gmail.com>
2021-08-05 00:54:45 -0700

    Update client_helper.py

--- client_helper.py
+++ client_helper.py
@@ -5,11 +5,17 @@ import numpy as np
 
 IMAGE_DIR = '/home/sensei/sensei-debug/extracts'
 IMG_COUNT = 36
+NUM_STEPS = 10
 
 step = 0
 step_pretty = str(step).zfill(6)
 
-while True:
+total_time = 0
+
+start = time.time()
+
+## transfer images for 
+while steps < NUM_STEPS + 1:
     images = glob(IMAGE_DIR + '/*')
     if any(step_pretty in i for i in images):
         current = [step_pretty in i for i in images]
@@ -18,12 +24,17 @@ while True:
             if truth_counts[1][1] == IMG_COUNT:
 
                 print('calling cinema client')
-
-                start = time.time()
+#                 start = time.time()
                 os.system('python3 /home/sensei/cinema_transfer/cinema_client.py --name osc --input {}'.format(IMAGE_DIR))
 
                 elapsed = time.time() - start
-                print('time elapsed: ' + str(elapsed))
-
+                if step != 0:
+                    total_time += elapsed
+                start = time.time()
+            
                 step += 1
                 step_pretty = str(step).zfill(6)
+
+             
+print('total time: ' + str(total_time))
+print('average per timstep: ' + str(total_time / NUM_STEPS))
