dc5c8785f5aee669898aaca1b4617139ae14121e
inealey <isaacnealey@gmail.com>
2021-08-05 00:44:32 -0700

    Update client_helper.py

--- client_helper.py
+++ client_helper.py
@@ -1,6 +1,7 @@
 import os
 from glob import glob
 import time
+import numpy as np
 
 IMAGE_DIR = '/home/sensei/sensei-debug/extracts'
 IMG_COUNT = 36
@@ -13,18 +14,16 @@ while True:
     if any(step_pretty in i for i in images):
         current = [step_pretty in i for i in images]
         truth_counts = np.unique(current, return_counts=True)
-        if len(truth_counts[1] > 1):
-            if np.unique(current, return_counts=True)[1][1] == IMG_COUNT:
-            
+        if len(truth_counts[1]) > 1:
+            if truth_counts[1][1] == IMG_COUNT:
+
                 print('calling cinema client')
-            
+
                 start = time.time()
-            
-                os.system('python3 cinema_client.py --name osc --input %'.format(IMAGE_DIR))
-            
+                os.system('python3 /home/sensei/cinema_transfer/cinema_client.py --name osc --input {}'.format(IMAGE_DIR))
+
                 elapsed = time.time() - start
-                print('time elapsed: ' + str(elapsed)) 
- 
+                print('time elapsed: ' + str(elapsed))
+
                 step += 1
                 step_pretty = str(step).zfill(6)
-            
